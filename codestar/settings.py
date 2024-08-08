74ef29be321ddd2c93494efb02db72c977f2097f
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-08-08 09:49:21 +0000

    debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('#i^gefz3#(w_j_b3i0za+clypo9^q-714zt#l&ds4q+$^5ra0#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nomeski80-djangoblog2-5jykthe2z7t.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
