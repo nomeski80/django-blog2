93dd2e95d6c7661e8c4db7f3fdcc17817a3d432a
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-08-12 02:57:37 +0000

    add post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nomeski80-djangoblog2-5jykthe2z7t.ws.codeinstitute-ide.net' ,'.herokuapp.com']
 
